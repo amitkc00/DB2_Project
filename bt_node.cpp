503837b91953a0a9f12caa7a01293d924d00b9b1
blrg891 <michael_feole1@student.uml.edu>
2013-02-18 10:32:51 -0500

    added bt_node.cpp - added To Do list (add accessor methods)

---
+++ bt_node.cpp
@@ -0,0 +1,24 @@
+#include "bt_node.h"
+
+
+//TO DO - implement functions below. 
+
+/*
+BtreeNode();
+
+~BtreeNode();
+
+int get_keyCount();
+
+void set_keyCount(int);
+
+//set type of node
+void  set_type(int);
+
+//get type of node. used while insertion, deletion or scan of record
+int get_type();
+
+BtreeNode* get_parentPtr();
+
+void set_parentPtr(BtreeNode *pointer);
+*/
