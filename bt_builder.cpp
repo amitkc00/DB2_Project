cd62f2ecebfa2a1d5449d18e42d9f156e5afa88f
Amit Choudhary <mr.amitkc@gmail.com>
2013-03-06 22:22:48 -0500

    New Files Created

---
+++ bt_builder.cpp
@@ -0,0 +1,37 @@
+// File Name : bt_builder.cpp
+// Date : 03/06/2013
+// Team : Amit & Mike
+
+
+#include "bt_builder.h"
+
+Status insertBuilderKey(KeyId) {
+
+
+}
+
+Status deleteBuilderKey(KeyId) {
+
+}
+
+Status splitNode(KeyId, KeyId *&, BtreeNode *, BtreeNode *&, BtreeNode *&) {
+
+
+}
+
+
+BtreeScan* openScan(KeyId lo_key, KeyId hi_key) {
+
+}
+
+Status findStartPosition(BtreeScan* , KeyId) {
+
+}
+
+Status destroyBtree () {
+
+}
+
+
+
+
---
+++ bt_leaf.cpp
@@ -0,0 +1,8 @@
+// File name : bt_leaf.cpp
+// Date : 03/06/3013
+// Team : Amit & Mike
+
+
+# include "bt_leaf.h"
+
+
