5bd982707b985eadae8fd39ab1b89c414d3a7bd0
Amit Choudhary <mr.amitkc@gmail.com>
2013-03-19 21:50:50 -0400

    bt_leaf.cpp & bt_builder.cpp updated.

--- bt_builder.cpp
+++ bt_builder.cpp
@@ -5,11 +5,30 @@
 
 #include "bt_builder.h"
 
+
 BtreeBuilder::~BtreeBuilder(){
   destroyBtree();
 }
 
 Status BtreeBuilder::insertBuilderKey(KeyId) {
+
+//AMIT
+Status insertBuilderKey(KeyId KeyValue) {
+	//Step 1 : Find the Child Pointer the KeyId needs to be inserted.
+	const int type = get_type(root);
+	if (type == LEAF) {
+		// Traverse this node and insert your value
+		root->insertKey(KeyValue,root->get_keyCount()) ; // As defined in bt_node.cpp
+		
+	}
+	else {
+			
+	}
+	
+	
+	//Step 2 : If the Child Pointer is of type LEAF, then insert it using leaf insertKey() else return error LEAF IS FULL
+
+
 
 
 }
--- bt_leaf.cpp
+++ bt_leaf.cpp
@@ -20,4 +20,71 @@ Status BtreeLeaf::searchKey(KeyId, int){
   return OK;
 }
 
+/*
+public:
+        BtreeLeaf()
+        {
+                set_type(LEAF);
+                set_keyCount(0);
+                //include all other initializations here
+        };
+
+        ~BtreeLeaf();
+
+        //insert to node, key of type KeyId and keyCount-int
+        Status insertKey(KeyId, int);
+
+        Status deleteKey(KeyId, int);
+
+        Status searchKey(KeyId, int);
+
+
+*/
+
+
+BtreeLeaf::set_type(LEAF) {
+
+}
+
+BtreeLeaf::set_keyCount(0){
+
+}
+
+BtreeLeaf::~BtreeLeaf() {
+
+}
+
+//AMIT
+Status BtreeLeaf::insertKey(KeyId KeyValue, int KeyCount) {
+	if(KeyCount == MAX_NUM_KEYS)	
+		return LEAF_IS_FULL;
+	else {
+		for(int i = 0; i< MAX_NUM_KEYS;i++) {
+			if(KeyValue <= key[i]) {
+				int shift = MAX_NUM_KEYS - (i + 1);
+				int j = 1;
+				while(j != shift ) {
+					key[MAX_NUM_KEYS-j] = key[MAX_NUM_KEYS-j-1]
+					j++;
+				}
+				key[i]=KeyValue;
+				set_keyCount(KeyCount+1);
+			}	
+		}
+	}
+	return OK;
+}
+
+Status BtreeLeaf::deleteKey(KeyId, int) {
+	return OK;
+
+}
+
+Status BtreeLeaf::searchKey(KeyId, int) {
+
+}
+
+
+
+
 
