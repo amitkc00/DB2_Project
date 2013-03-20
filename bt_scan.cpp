eee44ab25c9989a7686751f463f6cfd43f99be17
blrg891 <michael_feole1@student.uml.edu>
2013-03-19 20:56:01 -0400

    Completed functionality for bt_scan

--- bt_scan.cpp
+++ bt_scan.cpp
@@ -1,29 +1,6 @@
 #include "bt_scan.h"
 #include "bt_errors.h"
 
-
-/*
-        BtreeNode *leaf;          // leaf page containing current record
-
-        int pos;                //This will be the current position of key which we need to scan
-                                //This will be initially set to lo_key value and set to net key, each time we scan a key
-
-        KeyId endKey;          // if NULL, then go all the way right
-                                // else, stop when current key > this value.
-
-         ~BtreeScan();
-
-        BtreeNode* get_leaf();
-        Status set_leaf(BtreeNode *);
-        int get_pos();
-        Status set_pos(int);
-        KeyId get_endKey();
-        Status set_endKey(KeyId);
-        // get the next record at position "pos" from node pointed by "leaf".
-        Status getNext(KeyId *key);
-
-*/
-
 BtreeScan::~BtreeScan(){
   //cleanup
 }
@@ -59,13 +36,51 @@ Status BtreeScan::getNext(KeyId *key){
 
   //we are in node "leaf" at position "pos" 
   //based on the end key, we iterate through
-  // if endKey is null, the node is full so we go to the end
+  // if endKey is null, then we go to the end
   // otherwise we stop when the current key > endKey
 
-  // we get the next leaf node by saving the current key, going to the parent,
-  // seeing if it has the pointer to the next leaf, if not keep going up until
-  // we find the next leaf node which has the next key.
+  *key = leaf->getKey(pos);
+  pos++;
+  if(pos > leaf->get_keyCount()){
+    //find the next leaf
+    BtreeNode *temp = leaf->get_parentPtr();
+    int i;
+    while(temp != NULL){
+      //while we are not trying to go to the root's parent
 
-  return OK;
+      for(i = 0; i > MAX_NUM_PTRS; i++){
+	//iterate through all pointers
+	if( (temp->getPtr(i) == leaf) && (temp->getPtr(i + 1) != NULL) ){
+	  //if we find a match and there is another leaf pointer in this node
+	  leaf = temp->getPtr(i + 1);
+	  while(leaf->get_type() != LEAF){
+	    leaf = leaf->getPtr(0);
+	  }
+	  pos = 0;
+	  //if we have a valid end key and have reached it, return done
+	  if( (endKey != NULL) && (leaf->getKey(pos) > endKey) )
+	    return DONE;
+	  //if we found the next leaf return ok 
+	  return OK;
+	}
+      }
+
+      if(i == MAX_NUM_PTRS){
+	leaf = temp;
+	temp = leaf->get_parentPtr();
+      }
+
+    }
+
+  } else {
+    //if we have a valid end key and have reached it, return done
+    if( (endKey != NULL) && (leaf->getKey(pos) > endKey) )
+      return DONE;
+    //if we have not overstepped the bounds of the node we return ok
+    return OK;
+  }
+
+  //we overstepped our current node and cannot find the next leaf
+  return NO_MORE_KEYS;
 }
 
